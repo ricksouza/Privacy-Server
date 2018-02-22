74dc17fb4e1f683fbc612b1219619128b7fc25e5
Rick <ricksouza87@gmail.com>
2018-02-22 09:18:01 -0300

    Initial commit.

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lxmlrpc -lxmlrpc_server_abyss -lxmlrpc_server
+
